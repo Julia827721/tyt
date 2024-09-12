afb61f176756725130ebd92308c92594c049bc25
Julia827721 <shevnina.juli@yandex.ru>
2024-09-12 23:49:37 +0300

    Add files via upload

---
+++ scripts/burger.js
@@ -0,0 +1,15 @@
+console.log('burger');
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+
+function burgerClick()
+{
+    document.querySelector('.nav').classList.toggle('nav-active');
+    document.querySelector('.burger').classList.toggle('active');
+    navLinks.forEach((link, index)=>{
+    })
+}
+
+//American Typewriter, serif
+
